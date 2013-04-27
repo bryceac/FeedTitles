57ab3fb620646d5bbcf0748caf050e65390814a8
Bryce <tonyhawk2100@gmail.com>
2013-04-27 15:51:54 -0700

    fix tag feeds

--- feeds.php
+++ feeds.php
@@ -10,10 +10,10 @@ class Feeds
 		if ( Controller::get_action() == 'tag_collection' )
 		{
 			// if the condition is true, the following appends the tag name with the name of the blog
-			$xml->title = ucwords(htmlentities(Tags::get_by_slug($tag))->term_display) . ' - ' . Utils::htmlspecialchars( Options::get( 'title' ) );
+			$xml->title = ucwords(htmlentities(Tags::get_by_slug($tag)->term_display)) . ' - ' . Utils::htmlspecialchars( Options::get( 'title' ) );
 
 			// the following changes the description, or subtitle, as the ATOM standard calls it, and makes it reflect the feed
-			$xml->subtitle = ' posts on ' . htmlentities(Tags::get_by_slug($tag))->term_display . ' from ' . Utils::htmlspecialchars( Options::get( 'title' ) );
+			$xml->subtitle = ' posts on ' . htmlentities(Tags::get_by_slug($tag)->term_display) . ' from ' . Utils::htmlspecialchars( Options::get( 'title' ) );
 		}
 	} // end function
 	
